18bfc11a3058a94892d3bce1671354d88e2756ad
bys <410389821@qq.com>
2019-01-16 15:15:53 +0800

    增加ignore文件

--- PrintPDF/basicMain.cpp
+++ PrintPDF/basicMain.cpp
@@ -15,18 +15,7 @@ BasicMain::~BasicMain()
 
 void BasicMain::service_thread_func()
 {
-	std::ofstream ofs("\\custom_service.txt", std::ios::app);
-	if (!ofs)
-	{
-		return;
-	}
-	int i = 0;
-	while (i < 100)
-	{
-		ofs << "µÚ " << ++i << " ´Î" << std::endl;
-		Sleep(1000);
-	}
-	ofs.close();
+	
 }
 
 void BasicMain::OnStart(DWORD argc, TCHAR * argv[])
